45fb74a098f09caff63cdc4e75df77727752143f
evasalgado <imviarsha@gmail.com>
2023-11-03 12:58:08 -0600

    primer commit: creacion de proyecto

---
+++ Lab4P2_Grupo/src/lab4p2_grupo/Lab4P2_Grupo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4p2_grupo;
+
+/**
+ *
+ * @author evaja
+ */
+public class Lab4P2_Grupo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
